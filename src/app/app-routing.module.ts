68e9518486f19e3c04d5e9ed33b09fe631cc0995
gTeran12 <gteran@espol.edu.ec>
2023-08-01 10:43:49 -0400

    Creado hasta los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
+//  *Importacion de los componentes main, report y about
+
+import { MainComponent } from './pages/main/main.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ReportComponent } from './pages/report/report.component';
+
+
+
 const routes: Routes = [];
 
 @NgModule({
